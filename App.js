604bdd1c564a367242d89253a3b27416546b8b64
AlbertoCortina <albertocortina96@gmail.com>
2018-04-20 22:06:01 +0200

    .

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import I18n from './app/resources/i18n/i18n'
-import Styles from './app/resources/styles/styles'
 
 export default class App extends React.Component {
   render() {
     return (
-      <View>
-        <Text>{I18n.t('greeting')}Open up App.js to start working on your app!</Text>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
@@ -15,4 +13,11 @@ export default class App extends React.Component {
   }
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
