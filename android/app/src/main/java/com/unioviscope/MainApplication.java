e476a07ada890b5ca3a4b261912bea441b509525
AlbertoCortina <albertocortina96@gmail.com>
2018-10-01 21:16:19 +0200

    .

--- android/app/src/main/java/com/unioviscope/MainApplication.java
+++ android/app/src/main/java/com/unioviscope/MainApplication.java
@@ -3,6 +3,7 @@ package com.unioviscope;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import org.reactnative.camera.RNCameraPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RNCameraPackage(),
             new SplashScreenReactPackage(),
             new RNI18nPackage()
